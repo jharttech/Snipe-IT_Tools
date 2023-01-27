c00d83297b9980221134dc1b4d0baae9b3b7abb3
Jason Hartgraves <jharttech@hotmail.com>
2023-01-27 12:22:01 -0600

    Troubleshooting db connections.

--- db_tools/move_users_to_new_ou_in_DB.py
+++ db_tools/move_users_to_new_ou_in_DB.py
@@ -4,7 +4,11 @@ from helper_tools import misc
 
 class Get_Db_Info:
     def __init__(self,db_info):
-        return db_info
+        self.return_info_list(db_info)
+
+    def return_info_list(self,db_info):
+        self.db_info = db_info
+        return self.db_info
 
     @classmethod
     def get(cls):
