24bd42fec5eee744faab3b2d47110f38c8cba158
Jason Hartgraves <jharttech@hotmail.com>
2023-01-27 16:35:12 -0600

    rough pass on setting up db connection.

--- db_tools/move_users_to_new_ou_in_DB.py
+++ db_tools/move_users_to_new_ou_in_DB.py
@@ -2,25 +2,8 @@ import subprocess
 from helper_tools import misc
 
 
-class Get_Db_Info:
-    def __init__(self,db_info):
-        misc.connect_to_db(db_info)
-
-    @classmethod
-    def get(cls):
-        hostname = input("\nPlease enter the host of the Database: ")
-        dbase = input("\nPlease enter the database you wish to use: ")
-        username = input("\nPlease enter the user for the Database: ")
-        # Implenent way to obfuscate
-        passw = input("Please enter the Database password: ")
-        db_info = [hostname,dbase,username,passw]
-        return cls(db_info)
-
-
-
 def main():
-    db_info = Get_Db_Info.get()
-    misc.connect_to_db(db_info)
+    misc.connect_to_db()
 
 
 if __name__ == "__main__":
