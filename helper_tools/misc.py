87d90660d0f53e29833e418de7c4eaa28f113876
Jason Hartgraves <jharttech@hotmail.com>
2023-01-27 16:34:55 -0600

    rough pass on setting up db connection.

--- helper_tools/misc.py
+++ helper_tools/misc.py
@@ -21,14 +21,20 @@ class Setup:
             create = subprocess.Popen(["touch",str(i)], stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
 
 def connect_to_db(db_info):
+    hostname = input("\nPlease enter the host of the Database: ")
+    dbase = input("\nPlease enter the database you wish to use: ")
+    username = input("\nPlease enter the user for the Database: ")
+    # Implenent way to obfuscate
+    passw = input("Please enter the Database password: ")
     try:
         db = mysql.connector.connect(
-            host = db_info[0],
-            database = db_info[1],
-            user = db_info[2],
-            password = db_info[3]
+            host = hostname,
+            database = dbase,
+            user = username,
+            password = passw
         )
         if db.is_connected():
+            print("Connection to Database successful!")
             return [True,db]
     except mysql.connector.Error as e:
         return [e,None]
