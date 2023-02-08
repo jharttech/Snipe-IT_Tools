00eca9089c73d625ce9bf9410ed7062565d50d07
Jason Hartgraves <jharttech@hotmail.com>
2023-02-08 14:44:43 -0600

    Adding backup db features.

--- db_tools/move_users_to_new_ou_in_DB.py
+++ db_tools/move_users_to_new_ou_in_DB.py
@@ -3,7 +3,11 @@ from helper_tools import misc
 
 
 def main():
-    misc.connect_to_db()
+    backup = input("\nDo you want to back up the database? ").lower()
+    if backup == "y":
+        misc.backup_db()
+    elif backup == "n":
+        misc.connect_to_db()
 
 
 if __name__ == "__main__":
