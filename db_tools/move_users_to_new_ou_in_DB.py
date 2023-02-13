f05dbdb80b8e01e78f53ade33c6d4cc87c2ddad4
Jason Hartgraves <jharttech@hotmail.com>
2023-02-13 11:53:27 -0600

    Debugging.

--- db_tools/move_users_to_new_ou_in_DB.py
+++ db_tools/move_users_to_new_ou_in_DB.py
@@ -4,10 +4,29 @@ from helper_tools import misc
 
 
 class Get_Current_Location_Of_Users:
-    def __init__(self):
-        locale_code_to_org_unit_map = {}
-        org_unit_to_locale_code_map = {}
-        #cursor = db.cursor()
+    def __init__(self,db):
+        self.locale_code_to_org_unit_map = {}
+        self.org_unit_to_locale_code_map = {}
+        self.cursor = db.cursor()
+        
+        self.get_locations(self.cursor,self.locale_code_to_org_unit_map,self.org_unit_to_locale_code_map)
+
+    def get_locations(self,cursor,locale_code_to_org_unit_map,org_unit_to_locale_code_map):
+        self.cursor = cursor
+        self.records = self.cursor.fetchall()
+        self.locale_code_to_org_unit_map = dict(locale_code_to_org_unit_map)
+        self.org_unit_to_locale_code_map = dict(org_unit_to_locale_code_map)
+        
+        for row in self.records:
+            self.locale_code_to_org_unit_map.update({row[0] : row[1]})
+            self.org_unit_to_locale_code_map.update({row[1] : row[0]})
+
+        if(len(self.locale_code_to_org_unit_map) != 0) and (len(self.org_unit_to_locale_code_map) != 0):
+            self.cursor.close()
+            return [self.locale_code_to_org_unit_map,self.org_unit_to_locale_code_map]
+        
+    
+
 
 
 def main():
@@ -24,7 +43,8 @@ def main():
         connect = misc.connect_to_db()
         db = connect
 
-    #Get_Current_Location_Of_Users()
+    needed_dicts = Get_Current_Location_Of_Users(db)
+    print(needed_dicts[0] + "\n" + needed_dicts[1])
 
     
 
